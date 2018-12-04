7afd60827e124c7a2047249168c80e617b9d6d78
marianof2000 <mariano.francisco@gmail.com>
2018-12-04 12:10:32 -0300

    Update recorder.py

--- recorder.py
+++ recorder.py
@@ -13,11 +13,53 @@ import os
 import commands
 import glob
 from datetime import datetime
+from time import sleep
 
-primera = False
-
+# primera = False
 while True:
-    if primera:
+    fecha = str(datetime.now())[:19]
+    hora = int(fecha[11:13]+fecha[14:16])
+    # print(hora)
+    if (hora>=800 and hora<=1050) or (hora>=1110 and hora<=1630):
+        CHUNK = 1024
+        FORMAT = pyaudio.paInt16
+        CHANNELS = 2
+        RATE = 22000
+        RECORD_SECONDS = 300
+
+        p = pyaudio.PyAudio()
+
+        stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+        print("* recording")
+
+        WAVE_OUTPUT_FILENAME = fecha+".wav"
+        mp3file = fecha+".mp3"
+
+        frames = []
+
+        for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+            data = stream.read(CHUNK)
+            frames.append(data)
+
+        print("* done recording")
+
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
+        wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(p.get_sample_size(FORMAT))
+        wf.setframerate(RATE)
+        wf.writeframes(b''.join(frames))
+        wf.close()
+
+        #if primera:
         try:
             sound = pydub.AudioSegment.from_wav(WAVE_OUTPUT_FILENAME)
             sound.export(mp3file, format="mp3", bitrate="128k")
@@ -26,54 +68,8 @@ while True:
         else:
             os.remove(WAVE_OUTPUT_FILENAME)
             print("* MP3 Listo")
-    primera = True
+        #primera = True
 
-    CHUNK = 1024
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 2
-    RATE = 22000
-    RECORD_SECONDS = 300
-
-    p = pyaudio.PyAudio()
-
-    stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=CHUNK)
-
-    print("* recording")
-
-    fecha = str(datetime.now())[:19]
-    WAVE_OUTPUT_FILENAME = fecha+".wav"
-    mp3file = fecha+".mp3"
-
-    frames = []
-
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        frames.append(data)
-
-    print("* done recording")
-
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-
-    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
-    wf.writeframes(b''.join(frames))
-    wf.close()
-
-    """ try:
-        sound = pydub.AudioSegment.from_wav(WAVE_OUTPUT_FILENAME)
-        sound.export(mp3file, format="mp3", bitrate="128k")
-    except IOError:
-        print 'cannot open'
     else:
-        os.remove(WAVE_OUTPUT_FILENAME)
-        print("* MP3 Listo")
-    """
-    # commands.getoutput('lame -V0 -h -b 128 --vbr-new WAVE_OUTPUT_FILENAME mp3file')
+        print("Esperando: "+str(hora))
+        sleep(60)
